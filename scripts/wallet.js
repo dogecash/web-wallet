379dc36cfe6a570ce6638adde1bad8a31f1c42b3
JSKitty <loopylew113@protonmail.com>
2021-02-23 15:49:23 +0000

    Hide import menu after imports
    Also clears the input text to hide the key!

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -96,6 +96,8 @@ importWallet = function (newWif = false) {
     //Wallet Import Format to Private Key
     var privateKeyWIF = newWif || document.getElementById("privateKey").value;
     privateKeyForTransactions = privateKeyWIF;
+    document.getElementById("privateKey").value = "";
+    toggleWallet();
     var byteArryConvert = from_b58(privateKeyWIF, MAP)
     var droplfour = byteArryConvert.slice(0, byteArryConvert.length - 4);
     var key = droplfour.slice(1, droplfour.length);
@@ -170,6 +172,7 @@ importWallet = function (newWif = false) {
     // Set view key as public and refresh QR code
     viewPrivKey = true;
     toggleKeyView();
+
     // Update identicon
     document.getElementById("identicon").dataset.jdenticonValue = publicKeyForNetwork;
     jdenticon();
